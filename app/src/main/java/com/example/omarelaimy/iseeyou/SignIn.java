4e5f5aaa5d47f6b1b985448551df6cb157653730
Omar El-Elaimy <omar.elaimy94@gmail.com>
2017-04-26 04:05:55 +0200

    Added Backend functionality for signup Some bugs in sign in not solved yet

--- app/src/main/java/com/example/omarelaimy/iseeyou/SignIn.java
+++ app/src/main/java/com/example/omarelaimy/iseeyou/SignIn.java
@@ -9,6 +9,23 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
 
 
@@ -21,6 +38,9 @@ public class SignIn extends AppCompatActivity {
     private Button btnSignIn;
     private View EmailSeparator,PasswordSeparator;
     private EditText Email,Password;
+    private static final String TAG = "LoginActivity";
+    private static final String URL_FOR_LOGIN = "https://icu.000webhostapp.com/login.php";
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Making notification bar transparent
@@ -45,6 +65,9 @@ public class SignIn extends AppCompatActivity {
         Email = (EditText) findViewById(R.id.email_text);
         Password = (EditText) findViewById(R.id.password_text);
 
+        //Progress dialog to show when accessing the database
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
 
         ChangeSeparatorStatus(Email,EmailSeparator);
         ChangeSeparatorStatus(Password,PasswordSeparator);
@@ -59,14 +82,86 @@ public class SignIn extends AppCompatActivity {
             }
         });
 
-    //If Sign in is pressed, go to choose profile page
+    //If Sign in is pressed, check credentials if good, go to choose profile
     btnSignIn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(SignIn.this,ChooseProfile.class));
-            finish();
-        }
-    });
+            @Override
+            public void onClick(View view)
+            {
+                loginUser(Email.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void loginUser( final String email, final String password)
+    {
+        // Tag used to cancel the request
+        String cancel_req_tag = "login";
+        progressDialog.setMessage("Logging you in...");
+        showDialog();
+        StringRequest strReq = new StringRequest(Request.Method.POST, URL_FOR_LOGIN, new Response.Listener<String>()
+        {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+                hideDialog();
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    if (!error) {
+                        String user = jObj.getJSONObject("user").getString("name");
+                        // Launch Choose profile activity
+                        Intent intent = new Intent(SignIn.this,ChooseProfile.class);
+                        intent.putExtra("username", user);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else
+                    {
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+        // Adding request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq,cancel_req_tag);
+    }
+
+    //Function for showing the progress dialog
+    private void showDialog()
+    {
+        if (!progressDialog.isShowing())
+            progressDialog.show();
+    }
+    //Function for hiding the progress dialog
+    private void hideDialog()
+    {
+        if (progressDialog.isShowing())
+            progressDialog.dismiss();
     }
 
     private void ChangeSeparatorStatus(final EditText editText, final View view)
--- app/src/main/java/com/example/omarelaimy/iseeyou/SignUp.java
+++ app/src/main/java/com/example/omarelaimy/iseeyou/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.omarelaimy.iseeyou;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,17 +12,36 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Omar on 3/15/2017.
  */
 
 public class SignUp extends AppCompatActivity {
+    private static final String TAG = "RegisterActivity";
+    private static final String URL_FOR_REGISTRATION = "https://icu.000webhostapp.com/register.php";
+    ProgressDialog progressDialog;
     private TextView btnAlreadyuser;
     private EditText Name,Email,Password,ConfirmPass;
     private View NameSeparator,EmailSeparator,PasswordSeparator,ConfirmPassSeparator;
+    private Button btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Making notification bar transparent
@@ -36,13 +56,8 @@ public class SignUp extends AppCompatActivity {
         //Get the already a user button to navigate to sign in page
         btnAlreadyuser = (TextView) findViewById(R.id.already_user);
 
-        btnAlreadyuser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchSignin();
-            }
-        });
-
+        //Get the sign up button
+        btnSignup = (Button) findViewById(R.id.signup2);
 
         //Get the Separators..
         NameSeparator = (View) findViewById(R.id.name_separator);
@@ -52,7 +67,6 @@ public class SignUp extends AppCompatActivity {
 
         //Get the Edit Texts..
         Name = (EditText) findViewById(R.id.name_text);
-
         Email = (EditText) findViewById(R.id.email_text);
         Password = (EditText) findViewById(R.id.password_text);
         ConfirmPass = (EditText) findViewById(R.id.confirm_password);
@@ -62,6 +76,99 @@ public class SignUp extends AppCompatActivity {
         ChangeSeparatorColor(Email,EmailSeparator);
         ChangeSeparatorColor(Password,PasswordSeparator);
         ChangeSeparatorColor(ConfirmPass,ConfirmPassSeparator);
+
+        btnAlreadyuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchSignin();
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Signup(Name.getText().toString(),Email.getText().toString(),Password.getText().toString(),ConfirmPass.getText().toString());
+            }
+        });
+
+
+    }
+
+    private void Signup (final String name, final String email, final String password, final String confirmpassword)
+    {
+        // Tag used to cancel the request
+         String cancel_req_tag = "register";
+
+       // progressDialog.setMessage("We're signing you up :)");
+
+        //showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST, URL_FOR_REGISTRATION, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+              //  hideDialog();
+
+                try
+                {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    if (!error)
+                    {
+                        String user = jObj.getJSONObject("user").getString("name");
+                        Toast.makeText(getApplicationContext(), "Welcome " + user +", You are successfully Added!", Toast.LENGTH_SHORT).show();
+                        // Launch login activity
+                        Intent intent = new Intent(SignUp.this, SignIn.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else
+                    {
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Registration Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", name);
+                params.put("email", email);
+                params.put("password", password);
+                params.put("confirmpassword",confirmpassword);
+                return params;
+            }
+        };
+        // Adding request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq, cancel_req_tag);
+    }
+
+    private void showDialog() {
+        if (!progressDialog.isShowing())
+            progressDialog.show();
+    }
+
+    private void hideDialog() {
+        if (progressDialog.isShowing())
+            progressDialog.dismiss();
     }
 
     private void ChangeSeparatorColor(final EditText editText, final View view)
