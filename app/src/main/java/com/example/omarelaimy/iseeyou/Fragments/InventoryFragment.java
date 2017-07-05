f189b6d77294267731048b414e97e0e125c75a21
yasmine.saadawi <yasmine.saadawi@gmail.com>
2017-07-05 04:37:43 +0200

    My Inventory XML Dynamic Page

--- app/src/main/java/com/example/omarelaimy/iseeyou/Fragments/InventoryFragment.java
+++ app/src/main/java/com/example/omarelaimy/iseeyou/Fragments/InventoryFragment.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.text.GetChars;
 import android.text.Layout;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -15,6 +16,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.example.omarelaimy.iseeyou.Patient;
@@ -60,9 +63,15 @@ public class InventoryFragment extends Fragment {
         View  mainview  =  inflater.inflate(R.layout.fragment_inventory, container, false);
         LinearLayout mainlayout = (LinearLayout) mainview.findViewById(R.id.mainview_inventory);
 
-        LinearLayout pillLayout = CreatePillLayout();
+        for(int i = 0; i < 5; i++)
+        {
+            String name = "Pill Name " +Integer.toString(i);
+            LinearLayout pillLayout = CreatePillLayout(name,56,(i%2 +1));
+            LinearLayout separator = CreateSeparator();
+            mainlayout.addView(pillLayout);
+            mainlayout.addView(separator);
+        }
 
-        mainlayout.addView(pillLayout);
 
         return mainview;
     }
@@ -105,55 +114,107 @@ public class InventoryFragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 
-    public  LinearLayout CreatePillLayout()
+    public  LinearLayout CreatePillLayout(String PillName, int PillCount, int PillType)
     {
         //main layout for each pill
         LinearLayout PillLayout = new LinearLayout(getActivity());
         PillLayout.setOrientation(LinearLayout.HORIZONTAL);
 
+
         //Children of the pill layout
         LinearLayout pillIndicator = new LinearLayout(getActivity());
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(45, 250);
-        layoutParams.setMargins(20, 10, 20, 10);
-        pillIndicator.setLayoutParams(layoutParams);
+        LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(45, 250);
+        layoutParams1.setMargins(5, 10, 20, 10);
+        layoutParams1.weight = 1;
+        pillIndicator.setLayoutParams(layoutParams1);
         pillIndicator.setBackgroundColor(getResources().getColor(R.color.colorAccent));
         pillIndicator.setPadding(0,0,0,0);
+
 
         //Layout for pill Description
         LinearLayout pillInfo = new LinearLayout(getActivity());
         pillInfo.setOrientation(LinearLayout.VERTICAL);
         LinearLayout.LayoutParams layoutParams2 = new LinearLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.MATCH_PARENT);
-        layoutParams2.setMargins(80, 10, 20, 10);
+        layoutParams2.setMargins(30, 10, 20, 10);
+        layoutParams2.weight = 6;
         pillInfo.setLayoutParams(layoutParams2);
         //TextView for pillinfo
         TextView tv_pillname = new TextView(getActivity());
         TextView tv_pillcount = new TextView(getActivity());
         //Setting the info for the pill name.
-        tv_pillname.setText("Pill Name 1");
+        tv_pillname.setText(PillName);
         tv_pillname.setTextColor(getResources().getColor(R.color.welcomescreen));
         //tv_pillname.setTypeface(null, Typeface.BOLD);
         tv_pillname.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
 
-        tv_pillcount.setText("56 pills left");
+        tv_pillcount.setText(Integer.toString(PillCount) + " pills left");
         tv_pillcount.setTypeface(null,Typeface.ITALIC);
         tv_pillcount.setTextColor(getResources().getColor(R.color.greycolor));
         tv_pillcount.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
         pillInfo.addView(tv_pillname);
         pillInfo.addView(tv_pillcount);
 
+
         //ImageView for the icon
-        int src = R.drawable.circular_pill;
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
-        ImageView iv_pilltype = new ImageView(getActivity());
-        iv_pilltype.setImageResource(src);
-        iv_pilltype.setColorFilter(getResources().getColor(R.color.welcomescreen));
-        params.setMargins(100, 0, 0, 0);
-        iv_pilltype.setPadding(0,0,0,0);
-        iv_pilltype.setLayoutParams(params);
+        ImageView iv_pilltype = CreatePillView(PillType);
+
+        //Img for the X button
+        LinearLayout.LayoutParams layoutParams4 = new LinearLayout.LayoutParams(25, 25);
+        layoutParams4.weight = 1;
+        layoutParams4.setMargins(20, 0, 0,0);
+        int src1 = R.drawable.x;
+        ImageView deletebtn = new ImageView(getActivity());
+        deletebtn.setImageResource(src1);
+        deletebtn.setColorFilter(getResources().getColor(R.color.welcomescreen));
+        deletebtn.setPadding(0,0,0,0);
 
         PillLayout.addView(pillIndicator);
         PillLayout.addView(pillInfo);
         PillLayout.addView(iv_pilltype);
+        PillLayout.addView(deletebtn,layoutParams4);
+
         return PillLayout;
+    }
+
+    public LinearLayout CreateSeparator()
+    {
+        LinearLayout separator = new LinearLayout(getActivity());
+        LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, 2);
+        layoutParams1.setMargins(20, 20, 20, 20);
+        separator.setLayoutParams(layoutParams1);
+        separator.setBackgroundColor(getResources().getColor(R.color.greycolor));
+        return separator;
+
+    }
+
+    public ImageView CreatePillView(int PillType)
+    {
+        ImageView iv = new ImageView(getActivity());
+        int src = 0;
+        if(PillType == 1)
+        {
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(90,90);
+            src = R.drawable.circular_pill;
+            layoutParams.weight = 1;
+            iv.setImageResource(src);
+            iv.setColorFilter(getResources().getColor(R.color.welcomescreen));
+            layoutParams.setMargins(0, 60, 0, 0);
+            iv.setPadding(0,0,0,0);
+            iv.setLayoutParams(layoutParams);
+        }
+       else if(PillType == 2)
+        {
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(100,100);
+            src = R.drawable.capsule_pill;
+            layoutParams.weight = 1;
+            iv.setImageResource(src);
+            iv.setColorFilter(getResources().getColor(R.color.welcomescreen));
+            layoutParams.setMargins(0, 60, 0, 0);
+            iv.setPadding(0,0,0,0);
+            iv.setLayoutParams(layoutParams);
+
+        }
+        return iv;
+
     }
 }
